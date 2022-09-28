325ebff4560aee0e35b7f447b0e522b5c20c1782
cameronmcgeady <cameronmcgeady@gmail.com>
2022-09-28 18:30:28 +0100

    JAVA

---
+++ public/Javascript/time.js
@@ -0,0 +1,24 @@
+// OPEN THE CONSOLE BELOW TO SEE THE JAVASCRIPT RUNNING
+
+// find out what time it is
+
+var currenttime = new Date();
+var currenthour = currenttime.getHours();
+
+
+console.log(currenthour);
+
+// do a conditional
+
+var emailmessage = "You are <strong>OK</strong> to email me now";
+
+if (currenthour < 8 || currenthour > 16 || calObj.get(Calendar.DAY_OF_WEEK) != Calendar.FRIDAY ) {
+  emailmessage = "sorry this is out of work hours, try again " + Calendar.FRIDAY ;
+}
+
+var getheading = document.querySelector(".contact-me h2");
+console.log(getheading);
+getheading.insertAdjacentHTML(
+  "afterend",
+  '<p class="js-message">' + emailmessage + "</p>"
+);

3408d0363693a5a1d7cbd9b398f6819f92f11817
cameronmcgeady <cameronmcgeady@gmail.com>
2022-09-28 21:57:59 +0100

    buddy

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var contactRouter = require('./routes/contact');
+var buddyRouter = require('./routes/buddy');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/contact', contactRouter);
+app.use('/Buddy', buddyRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

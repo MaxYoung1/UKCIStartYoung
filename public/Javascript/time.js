a35f021bdb8fd4ce19781efe020d87c06846892e
cameronmcgeady <cameronmcgeady@gmail.com>
2022-09-28 19:19:05 +0100

    sorting stuff

--- public/Javascript/time.js
+++ public/Javascript/time.js
@@ -17,12 +17,12 @@ console.log(currenthour);
 var emailmessage = "You are <strong>OK</strong> to contact me now and I will respond shortly";
 
 if (currenthour < 9 || currenthour > 17 || daylist[day] != "Friday"  ) {
-  emailmessage = "Sorry it is currently out of our working hours, i will try respond on Friday 9am-5pm UK" ;
+  emailmessage = "Sorry it is currently out of our working hours so I will not be answering the phone, Please try on Friday 9am-5pm UK" ;
 }
 
 var getheading = document.querySelector(".contact-me h2");
 console.log(getheading);
 getheading.insertAdjacentHTML(
   "afterend",
-  '<p class="js-message"> <strong>' + emailmessage + "</strong></p>" 
+  '<p class="js-message"> <strong>' +emailmessage + "</strong></p>" 
 );

44ccde1d60d3317cb45c4d12c8224444135ab843
cameronmcgeady <cameronmcgeady@gmail.com>
2022-09-28 18:58:47 +0100

    f

--- public/Javascript/time.js
+++ public/Javascript/time.js
@@ -1,6 +1,9 @@
 // OPEN THE CONSOLE BELOW TO SEE THE JAVASCRIPT RUNNING
 
 // find out what time it is
+var day = today.getDay();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+
 
 var currenttime = new Date();
 var currenthour = currenttime.getHours();
@@ -12,8 +15,8 @@ console.log(currenthour);
 
 var emailmessage = "You are <strong>OK</strong> to email me now";
 
-if (currenthour < 8 || currenthour > 16 || calObj.get(Calendar.DAY_OF_WEEK) != Calendar.FRIDAY ) {
-  emailmessage = "sorry this is out of work hours, try again " + Calendar.FRIDAY ;
+if (currenthour < 8 || currenthour > 16 || daylist[day] != "Friday"  ) {
+  emailmessage = "sorry this is out of work hours, try again " + day ;
 }
 
 var getheading = document.querySelector(".contact-me h2");
